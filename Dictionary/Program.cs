0ba027dc8b8357ba700f7b7888a7d19e539650f2
mgapp <mgapp@MIKEGYO>
2025-06-03 18:16:08 -0400

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<int, string> lookup = new Dictionary<int, string>();
+            //Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+            //Dictionary<DateTime, string> keyValuePairs1 = new Dictionary<DateTime, string>();
+            //Dictionary<DateOnly, string> keyValuePairs2 = new Dictionary<DateOnly, string>();
+            //Dictionary<bool, string> keyValuePairs3 = new Dictionary<bool, string>();
+            //Dictionary<double, string> keyValuePairs4 = new Dictionary<double, string>();
+            //Dictionary<string, string> fish = new Dictionary<string, string>();
+            //fish["shark"] = "Ahhhhhhhh!";
+            //fish["catfish"] = "oh";
+            //string answer = Console.ReadLine();
+            //Console.WriteLine(fish[answer]);
+            Console.WriteLine("Please enter key: ");
+            string key = Console.ReadLine();
+            Console.WriteLine("Write Value");
+            string value = Console.ReadLine();
+            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+            keyValuePairs.Add(key, value);
+            Console.WriteLine($"Key/Value is {keyValuePairs}");
+        }
+    }
+}
